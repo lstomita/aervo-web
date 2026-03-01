106e1bc2d03546d24257b92ead226d05431c8e55
Lane <lanestomita@gmail.com>
2026-02-28 14:44:55 -1000

    2/28/2026 Updates to public trip webpage

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,12 @@ import './index.css'
 import Support from './Support' // <--- 1. Import the new page
 import Terms from './Terms'       // <--- ADD THIS IMPORT
 
+// Detect Safari specifically to fix its unique translucent UI issues
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+if (isSafari) {
+  document.documentElement.classList.add('is-safari');
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
